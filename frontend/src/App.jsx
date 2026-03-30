097cd16fdba6bf24344af4213710ee782a1f80fe
Naina Jaiswal <nainajais777@gmail.com>
2026-03-31 00:21:23 +0530

    Added frontend (React + Tailwind + DaisyUI)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+/*import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'*/
+import './App.css'
+import Navbar from './assets/Navbar'
+import Footer from './assets/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+     <h1 className="text-3xl font-bold underline text-red-500">
+        Hello Tailwind
+      </h1>
+
+    </>
+  )
+}
+
+export default App
